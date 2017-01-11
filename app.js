30be0d917d8dd2f14f1725bf977b5d2eac7e07ac
Fletcher Watson <fbwthe6@gmail.com>
2017-01-11 17:04:43 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+'use strict'
+
+function Person(){
+  this.name = null
+  this.state = null
+  this.age = null
+  this.handsome = null
+  this.salary = null
+}
+
+let jerry = new Person()
+
+jerry = {
+  name: 'jerry',
+  state: 'tn',
+  age: 38,
+  handsome: true,
+  salary: 600000,
+  bio: function() { return `My name is ${jerry.name} I'm from ${jerry.state} and I am ${jerry.age} years old. I make a fuckload of money, specifically ${jerry.salary} dollars per year.`
+  }
+}
+
+$('#greeting').text(jerry.bio())
+
+function getDayAndTime(){
+  var objToday = new Date(),
+	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+	dayOfWeek = weekday[objToday.getDay()],
+	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+	curMonth = months[objToday.getMonth()],
+	curYear = objToday.getFullYear(),
+	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+
+document.getElementById('greeting').textContent = today;
+  console.log(today)
+}
+
+getDayAndTime()
